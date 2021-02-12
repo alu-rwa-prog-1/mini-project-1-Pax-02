4f49ea6f512519f81c528b0170d9e3b371ee2aa7
pax_02 <p.hanyurwim@alustudent.com>
2021-02-12 10:39:59 +0200

    pizza section added

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -20,30 +20,35 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
         theme: ThemeData(fontFamily: 'Montserrat'),
         home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.greenAccent[400],
-            title: Center(child: Text("Bougie Food")),
-            actions: [
-              Icon(Icons.search,color: Colors.black87),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Icon(Icons.alarm,color: Colors.black87),
-              )
-            ],
-          ),
-          drawer: Drawer(
-            child: ListView(
-              children: <Widget>[
-                new UserAccountsDrawerHeader(
-                  accountName: Text("Peter John"),
-                  accountEmail: Text("Peter.johnn@work.com"),
-                  currentAccountPicture:
-                      CircleAvatar(backgroundImage: new NetworkImage("")),
+            appBar: AppBar(
+              backgroundColor: Colors.greenAccent[400],
+              title: Center(child: Text("Bougie Food")),
+              actions: [
+                Icon(Icons.search, color: Colors.black87),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Icon(Icons.alarm, color: Colors.black87),
                 )
               ],
             ),
-          ),
-          body: Padding(
+            drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  new UserAccountsDrawerHeader(
+                    accountName: Text("Peter John"),
+                    accountEmail: Text("Peter.johnn@work.com"),
+                    currentAccountPicture:
+                        CircleAvatar(backgroundImage: new NetworkImage("")),
+                  )
+                ],
+              ),
+            ),
+            body: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                Column(
+                  children: [Container(
+                    child: Padding(
             padding:
                 const EdgeInsets.symmetric(horizontal: 15.0, vertical: 25.0),
             child: Column(children: <Widget>[
@@ -174,6 +179,17 @@ class _MyAppState extends State<MyApp> {
                       child: Image.asset("assets/burger2.png",fit: BoxFit.fill,),
                       height: 150.0 ,
                       ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
+                      ),
                       Text("Pizza"),
                         ],),
                       ),
@@ -194,6 +210,17 @@ class _MyAppState extends State<MyApp> {
                       width: 150.0,
                       child: Image.asset("assets/burger1.png",fit: BoxFit.fill,),
                       height:150.0 ,
+                      ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
                       ),
                       Text("Smoothies"),
                       
@@ -217,6 +244,17 @@ class _MyAppState extends State<MyApp> {
                       height:150.0,
                       
                       ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
+                      ),
                       Text("Plate"),
                         ],),
                       ),
@@ -226,37 +264,181 @@ class _MyAppState extends State<MyApp> {
                   ],
                 ),
               ),
+              Container(child: Column(children: [
+                Text(
+                "Pizza Section",
+                style: Theme.of(context).textTheme.headline5.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    fontFamily: "Montserrat"),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                color: Colors.grey[200],
+                width: 500.0,
+                height:300.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Card(child: Column(children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(5.0,5.0,0.0,0.0),
+                          width: 120.0,
+                          height: 25.0,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
+                          child: Text("-10%",style: TextStyle(color: Colors.red,backgroundColor: Colors.green[100],fontSize:18.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                      width: 150.0,
+                      child: Image.asset("assets/pizza1.png",fit: BoxFit.fill,),
+                      height:150.0 ,
+                      ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
+                      ),
+                      Text("Burger"),
+                        ],),
+                      ),
+                      Card(child: Column(children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(5.0,5.0,0.0,0.0),
+                          width: 120.0,
+                          height: 25.0,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
+                          // child: Text("-2.5%",style: TextStyle(color: Colors.red,backgroundColor: Colors.green[100]),),
+                        ),
+                      ),
+                      SizedBox(
+                      width: 180.0,
+                      child: Image.asset("assets/pizza2.png",fit: BoxFit.fill,),
+                      height: 150.0 ,
+                      ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
+                      ),
+                      Text("Pizza"),
+                        ],),
+                      ),
+                      Card(child: Column(children: [
+
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(5.0,5.0,0.0,0.0),
+                          width: 120.0,
+                          height: 25.0,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
+                          // child: Text("-10%",style: TextStyle(color: Colors.red,backgroundColor: Colors.green[100]),),
+                        ),
+                      ),
+                      
+                      SizedBox(
+                      width: 150.0,
+                      child: Image.asset("assets/pizza3.png",fit: BoxFit.fill,),
+                      height:150.0 ,
+                      ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
+                      ),
+                      Text("Smoothies"),
+                      
+                        ],),
+
+                      ),
+                      Card(child: Column(children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(5.0,5.0,0.0,0.0),
+                          width: 120.0,
+                          height: 25.0,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white),
+                          child: Text("-2.5%",style: TextStyle(color: Colors.red,backgroundColor: Colors.green[100],fontSize:18.0),),
+                        ),
+                      ),
+                      SizedBox(
+                      width: 150.0,
+                      child: Image.asset("assets/pizza4.png",fit: BoxFit.fill,),
+                      height:150.0,
+                      
+                      ),
+                      Container(
+                        alignment: Alignment.bottomLeft,
+                        child: IconButton(
+                          highlightColor: Colors.red,
+                          icon: Icon(Icons.favorite_border_rounded),
+                          color: Colors.black,
+                          onPressed: () {
+                            color: Colors.red;
+                          },
+                        ),
+                      ),
+                      Text("Plate"),
+                        ],),
+                      ),
+                      
+                      
+                    
+                  ],
+                ),
+              ),
+              ],),)
             ]),
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text("Home"),
-                backgroundColor: Colors.green
-              ),
+                  )],
+                )
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    title: Text("Home"),
+                    backgroundColor: Colors.green),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.shop_rounded),
+                    title: Text("cart"),
+                    backgroundColor: Colors.green),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.account_circle),
+                    title: Text("Account"),
+                    backgroundColor: Colors.green)
+              ],
+              selectedItemColor: Colors.white,
+              backgroundColor: Colors.greenAccent[400],
+            )
 
-               BottomNavigationBarItem(
-                icon: Icon(Icons.shop_rounded),
-                title: Text("cart"),
-                backgroundColor: Colors.green
-              ),
-
-               BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                title: Text("Account"),
-                backgroundColor: Colors.green
-              )
-              
-            ],
-            selectedItemColor: Colors.white,
-            backgroundColor: Colors.greenAccent[400],
-            
-            
-          )
-
-          //
-        ));
+            //
+            ));
   }
 }
